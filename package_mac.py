8e40fff9ff94780c4dceec3115cffa40f2ff6938
morganq <morganquirk@gmail.com>
2021-10-31 12:07:27 -0700

    icon etc

--- package_mac.py
+++ package_mac.py
@@ -4,7 +4,7 @@ NAME = "HostileQuadrant"
 CONTENTS = "dist/%s.app/Contents/MacOS" % NAME
 
 os.system("rm -rf ./dist")
-os.system("pyinstaller -w -n \"%s\" -i assets/icon.icns --osx-bundle-identifier com.example.test main.py" % NAME)
+os.system("pyinstaller -w -n \"%s\" -i assets/icon4x.icns --osx-bundle-identifier com.example.test main.py" % NAME)
 os.system("mkdir %s/assets" % CONTENTS)
 os.system("mkdir %s/assets/buildings" % CONTENTS)
 os.system("mkdir %s/assets/upgrades" % CONTENTS)
@@ -18,3 +18,5 @@ os.system("cp assets/*.ttf %s/assets/" % CONTENTS)
 os.system("cp assets/*.wav %s/assets/" % CONTENTS)
 os.system("cp assets/*.ogg %s/assets/" % CONTENTS)
 os.system("cp levels/*.json %s/levels/" % CONTENTS)
+os.system("cp assets/*.json %s/assets/" % CONTENTS)
+os.system("cp assets/*.txt %s/assets/" % CONTENTS)
--- package_win.py
+++ package_win.py
@@ -4,7 +4,7 @@ NAME = "HostileQuadrant"
 CONTENTS = "dist\\%s" % NAME
 
 os.system("rmdir /Q /S dist")
-os.system('pyinstaller -w -n %s -i assets\\icon_2_256.ico main.py' % NAME)
+os.system('pyinstaller -w -n %s -i assets\\icon4x.ico main.py' % NAME)
 os.system("mkdir %s\\assets" % CONTENTS)
 os.system("mkdir %s\\assets\\sounds" % CONTENTS)
 os.system("mkdir %s\\levels" % CONTENTS)
@@ -13,3 +13,5 @@ os.system("cp assets\\*.ttf %s\\assets\\" % CONTENTS)
 os.system("cp assets\\sounds\\*.wav %s\\assets\\sounds\\" % CONTENTS)
 os.system("cp assets\\*.ogg %s\\assets\\" % CONTENTS)
 os.system("cp -r levels\\*.csv %s\\levels\\" % CONTENTS)
+os.system("cp -r assets\\*.json %s\\" % CONTENTS)
+os.system("cp -r assets\\*.txt %s\\" % CONTENTS)
